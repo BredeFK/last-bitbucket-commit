44855bd7c421b42c9195b3de3cabf5ce826f1890
BredeFK <BredeFK@stud.ntnu.no>
2017-11-06 00:40:05 +0100

    Added getfunc and made error message prettier

--- main.go
+++ main.go
@@ -6,6 +6,42 @@ import (
 	"fmt"
 	"os"
 )
+
+func getInfo(URL string){
+	/*
+	client := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, URL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "PushInfo")
+
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	json := Json{}
+	jsonErr := json.Unmarshal(body, &json)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return json
+	*/
+
+
+}
+
 func HandleBitbucket(w http.ResponseWriter, r *http.Request) {
 	// DECLARE IT'S A JSON FILE
 	http.Header.Add(w.Header(), "Content-type", "application/json")
@@ -14,17 +50,23 @@ func HandleBitbucket(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.Path, "/")
 
 	// THE DOMAIN HAS TO BE BITBUCKET
-	url := "test"
+	url := ""
 	if parts[2] == "bitbucket.org" {
 
 		// MAKE API URL TO GET JSON FROM THE REPO
 		url = "https://api.bitbucket.org/2.0/repositories/" + parts[3] + "/" + parts[4] + "/commits"
+		// PRINT API URL TO USER
+
+		// GET INFO FROM API SITE
+		// info := getInfo(url)
+
+		fmt.Fprint(w, url)
 	}else{
-		http.Error(w,"Domain has to be 'bitbucket.org'",http.StatusMethodNotAllowed)
+		// ERROR IF THE DOMAIN INS'T BITBUCKET
+		http.Error(w,"Domain can not be '" + parts[2] + "', it has to be 'bitbucket.org'",http.StatusMethodNotAllowed)
 	}
 
-	// PRINT API URL TO USER
-	fmt.Fprint(w, url)
+
 }
 
 func main(){
